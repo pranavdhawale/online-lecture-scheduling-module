d3b3d889c146a4b52d5c254ae0efdb57a11dc099
Pranav Dhawale <pranavdhawale19@gmail.com>
2022-12-10 11:33:53 +0530

    feat: connect with mongodb atlas

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,31 @@
+const dotenv = require('dotenv')
+
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
 
 const app = express()
 
 const port = 9000
+
+app.use(cors())
+
+app.use(bodyParser.json())
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }))
+
+mongoose.set('strictQuery', false);
+
+// connect to mongodb atlas
+const mongouri =  `mongodb+srv://admin:smsjNOMyw4nC4KQs@online-lecture-scheduli.woweejf.mongodb.net/?retryWrites=true&w=majority`
+mongoose.connect(mongouri, { useNewUrlParser: true });
+const connection = mongoose.connection;
+
+connection.once('open', function() {
+    console.log('MongoDB database connection established successfully!');
+    }
+);
 
 app.get('/', (req, res) => {
     console.log("Working");
