f781cebcae2fc1c47cb11cc7fdf25f00fb0cac15
Pranav Dhawale <pranavdhawale19@gmail.com>
2022-12-10 15:41:32 +0530

    feat: create [addInstructor, addCourse, allocateLecture] routes

--- backend/routes/admin.routes.js
+++ backend/routes/admin.routes.js
@@ -1,14 +1,27 @@
 const express = require('express')
 const router = express.Router();
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 // import admin controller
 const adminController = require('../controller/admin.controller')
 
-// test route
-router.get('/', (req, res) => {
-    console.log("Admin Testing");
-})
+// test
+router.get('/', adminController.test)
 
-router.get('/test', adminController.test)
+// register
+router.post('/register', adminController.registerAdmin)
+
+// login
+router.post('/login', adminController.loginAdmin)
+
+// add instructor
+router.post('/add-instructor', adminController.addInstructor)
+
+// add course
+router.post('/add-course', adminController.addCourse)
+
+// allocate lecture
+router.post('/allocate-lecture', adminController.allocateLecture)
 
 module.exports = router;
