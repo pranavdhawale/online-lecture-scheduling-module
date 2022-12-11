b4961019288cfda7f673f0dfcc2044bd0a8878ed
Pranav Dhawale <pranavdhawale19@gmail.com>
2022-12-11 22:21:22 +0530

    chore: add new buttons [view-instructors, view-courses, allocated-lecture]

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -18,15 +18,36 @@ export default function Dashboard() {
         <p>Username : {content.username}</p>
         <p>Name : {content.name}</p>
 
-        <NavLink to="/add-instructor">
-            <button className="border">ADD INSTRUCTOR</button>
-        </NavLink><br />
-        <NavLink to="/add-course">
-            <button className="border">ADD COURSE</button>
-        </NavLink><br />
-        <NavLink to="/allocate-lecture">
-            <button className="border">ALLOCATE LECTURE</button>
-        </NavLink>
+        <div>
+            <NavLink to="/add-instructor">
+                <button className="border">ADD INSTRUCTOR</button>
+            </NavLink>
+            <NavLink to="/view-instructors">
+                <button className="border">VIEW INSTRUCTORS</button>
+            </NavLink>
+        </div>
+
+        <br />
+        
+        <div>
+            <NavLink to="/add-course">
+                <button className="border">ADD COURSE</button>
+            </NavLink>
+            <NavLink to="/view-courses">
+                <button className="border">VIEW COURSES</button>
+            </NavLink>
+        </div>
+        
+        <br />
+        
+        <div>
+            <NavLink to="/allocate-lecture">
+                <button className="border">ALLOCATE LECTURE</button>
+            </NavLink>
+            <NavLink to="/allocated-lecture">
+                <button className="border">ALLOCATED LECTURE</button>
+            </NavLink>
+        </div>
         </>
     )
 }
