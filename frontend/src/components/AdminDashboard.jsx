9d01c104e9d7c5eb1ccf4c9d44a880bc3c903136
Pranav Dhawale <pranavdhawale19@gmail.com>
2022-12-11 22:26:29 +0530

    chore: different dashboard for admin

---
+++ frontend/src/components/AdminDashboard.jsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Navigate, useNavigate, NavLink } from "react-router-dom";
+
+export default function AdminDashboard() {
+    const [content, setContent] = useState({})
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        setContent(JSON.parse(sessionStorage.getItem("info")))
+        console.log(content);
+    }, [])
+
+    return(
+        <>
+        <p className="text-3xl">Dashboard</p>
+
+        <p>Username : {content.username}</p>
+        <p>Name : {content.name}</p>
+
+        <div>
+            <NavLink to="/add-instructor">
+                <button className="border">ADD INSTRUCTOR</button>
+            </NavLink>
+            <NavLink to="/view-instructors">
+                <button className="border">VIEW INSTRUCTORS</button>
+            </NavLink>
+        </div>
+
+        <br />
+        
+        <div>
+            <NavLink to="/add-course">
+                <button className="border">ADD COURSE</button>
+            </NavLink>
+            <NavLink to="/view-courses">
+                <button className="border">VIEW COURSES</button>
+            </NavLink>
+        </div>
+        
+        <br />
+        
+        <div>
+            <NavLink to="/allocate-lecture">
+                <button className="border">ALLOCATE LECTURE</button>
+            </NavLink>
+            <NavLink to="/allocated-lecture">
+                <button className="border">ALLOCATED LECTURE</button>
+            </NavLink>
+        </div>
+        </>
+    )
+}
