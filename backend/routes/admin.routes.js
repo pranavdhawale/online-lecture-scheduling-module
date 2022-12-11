7b5d136b2625d644249a887e8880cbfcebd50ac7
Pranav Dhawale <pranavdhawale19@gmail.com>
2022-12-11 22:12:05 +0530

    feat: new route to allocate lecture

--- backend/routes/admin.routes.js
+++ backend/routes/admin.routes.js
@@ -18,8 +18,14 @@ router.post('/login', adminController.loginAdmin)
 // add instructor
 router.post('/add-instructor', adminController.addInstructor)
 
+// get all instructor
+router.get('/instructor-list', adminController.getAllInstructors)
+
 // add course
 router.post('/add-course', adminController.addCourse)
+
+// get all courses
+router.get('/course-list', adminController.getAllCourses)
 
 // allocate lecture
 router.post('/allocate-lecture', adminController.allocateLecture)
