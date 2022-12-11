5b976cf98ae5378f6448365f2cb2077f6bac65fc
Pranav Dhawale <pranavdhawale19@gmail.com>
2022-12-12 00:05:36 +0530

    feat: add [username, password] fields in instructor model

--- backend/model/Instructor.model.js
+++ backend/model/Instructor.model.js
@@ -2,6 +2,14 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const Instructor = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
     name: {
         type: String,
         required: true
