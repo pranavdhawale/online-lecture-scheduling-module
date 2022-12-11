3b1898b8d3a40ac929fcda60bc55e7a85c94e058
Pranav Dhawale <pranavdhawale19@gmail.com>
2022-12-11 22:13:01 +0530

    feat: create routes for pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import LoginPage from "./components/LoginPage";
+import Dashboard from "./components/Dashboard";
+import AddInstructor from "./components/AddInstructor";
+import AddCourse from "./components/AddCourse";
+import AllocateLecture from "./components/AllocateLecture";
 
 export default function App() {
   return(
     <>
-    <p>Hello World</p>
+    <Routes>
+      <Route exact path="/" element={< LoginPage />}/>
+      <Route exact path="/dashboard" element={<Dashboard />}/>
+      <Route exact path="/add-instructor" element={<AddInstructor />}/>
+      <Route exact path="/add-course" element={<AddCourse />}/>
+      <Route exact path="/allocate-lecture" element={<AllocateLecture />}/>
+    </Routes>
     </>
   )
 }
