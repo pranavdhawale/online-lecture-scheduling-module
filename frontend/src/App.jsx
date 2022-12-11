2f3ef3acf8d7d971ad9e0f84400a1473cec9ae17
Pranav Dhawale <pranavdhawale19@gmail.com>
2022-12-11 23:37:55 +0530

    feat: create routes for each page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,26 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import LoginPage from "./components/LoginPage";
-import Dashboard from "./components/Dashboard";
-import AddInstructor from "./components/AddInstructor";
-import AddCourse from "./components/AddCourse";
-import AllocateLecture from "./components/AllocateLecture";
+import AdminDashboard from "./components/AdminDashboard";
+import AddInstructor from "./components/admin/AddInstructor";
+import ViewInstructors from "./components/admin/ViewInstructors";
+import AddCourse from "./components/admin/AddCourse";
+import ViewCourses from "./components/admin/ViewCourse";
+import AllocateLecture from "./components/admin/AllocateLecture";
+import AllocatedLecture from "./components/admin/AllocatedLecture";
 
 export default function App() {
   return(
     <>
     <Routes>
       <Route exact path="/" element={< LoginPage />}/>
-      <Route exact path="/dashboard" element={<Dashboard />}/>
+      <Route exact path="/dashboard" element={<AdminDashboard />}/>
       <Route exact path="/add-instructor" element={<AddInstructor />}/>
+      <Route exact path="/view-instructors" element={<ViewInstructors />}/>
       <Route exact path="/add-course" element={<AddCourse />}/>
+      <Route exact path="/view-courses" element={<ViewCourses />}/>
       <Route exact path="/allocate-lecture" element={<AllocateLecture />}/>
+      <Route exact path="/allocated-lecture" element={<AllocatedLecture />}/>
     </Routes>
     </>
   )
